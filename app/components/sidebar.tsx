a59d6dc86915f405c7e4e312bdbd555c2e9d8ea0
GITHUB1LEADER <127388858+GITHUB1LEADER@users.noreply.github.com>
2023-07-02 13:58:06 +0800

    Update sidebar.tsx

--- app/components/sidebar.tsx
+++ app/components/sidebar.tsx
@@ -117,10 +117,10 @@ export function SideBar(props: { className?: string }) {
     >
       <div className={styles["sidebar-header"]} data-tauri-drag-region>
         <div className={styles["sidebar-title"]} data-tauri-drag-region>
-          ChatGPT Next
+          MOYULIU GPT
         </div>
         <div className={styles["sidebar-sub-title"]}>
-          Build your own AI assistant.
+          摸鱼刘测试！想了解相关内容，请点击下方GitHub图标，支持Yidadaa大佬！
         </div>
         <div className={styles["sidebar-logo"] + " no-dark"}>
           <ChatGptIcon />
